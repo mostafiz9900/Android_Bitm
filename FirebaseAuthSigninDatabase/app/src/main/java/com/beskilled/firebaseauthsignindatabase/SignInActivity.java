1e2ff49290a021c6e709d49722c5ad9ebac47f4a
mostafiz9900 <mostafiz9900@gmail.com>
2019-07-22 00:40:09 +0600

    real time database insert data

---
+++ FirebaseAuthSigninDatabase/app/src/main/java/com/beskilled/firebaseauthsignindatabase/SignInActivity.java
@@ -0,0 +1,67 @@
+package com.beskilled.firebaseauthsignindatabase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.beskilled.firebaseauthsignindatabase.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignInActivity extends AppCompatActivity {
+    private ActivitySignInBinding signInBinding;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        signInBinding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
+        setTitle("Sing In Activity");
+        init();
+        signInBinding.signInToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        signInBinding.signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = signInBinding.emailSingInEt.getText().toString().trim();
+                String password = signInBinding.passwordSingInEt.getText().toString().trim();
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void init() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+}
---
+++ FirebaseAuthSigninDatabase/app/src/main/java/com/beskilled/firebaseauthsignindatabase/SignUpActivity.java
@@ -0,0 +1,81 @@
+package com.beskilled.firebaseauthsignindatabase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.beskilled.firebaseauthsignindatabase.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class SignUpActivity extends AppCompatActivity {
+private ActivitySignUpBinding signUpBinding;
+private FirebaseAuth firebaseAuth;
+private DatabaseReference database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        signUpBinding= DataBindingUtil.setContentView(this,R.layout.activity_sign_up);
+init();
+        signUpBinding.signUpToSingInTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUpBinding.signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userRegistation();
+            }
+        });
+    }
+
+    private void init() {
+        firebaseAuth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance().getReference();
+    }
+
+    private void userRegistation() {
+        final String name=signUpBinding.nameSignUpEt.getText().toString().trim();
+        final String email=signUpBinding.emailSignUpEt.getText().toString().trim();
+        String password=signUpBinding.passwordSignUpEt.getText().toString().trim();
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                  String userId=firebaseAuth.getCurrentUser().getUid();
+                  DatabaseReference userRef=database.child("student").child(userId);
+                    HashMap<String,Object> student=new HashMap<>();
+                    student.put("name",name);
+                    student.put("email",email);
+                    userRef.setValue(student).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                finish();
+                                Intent intent = new Intent(getApplicationContext(),SignInActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                }else {
+                    Toast.makeText(getApplicationContext(), "Unsuccessful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
