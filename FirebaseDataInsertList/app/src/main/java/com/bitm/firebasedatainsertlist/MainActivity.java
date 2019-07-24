883ab44f5807ca88deca7fb1c8832e06f9e8e4a8
mostafiz9900 <mostafiz9900@gmail.com>
2019-07-24 11:58:05 +0600

    list view firebase

--- FirebaseDataInsertList/app/src/main/java/com/bitm/firebasedatainsertlist/MainActivity.java
+++ FirebaseDataInsertList/app/src/main/java/com/bitm/firebasedatainsertlist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bitm.firebasedatainsertlist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,11 +9,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.bitm.firebasedatainsertlist.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,10 +51,35 @@ private DatabaseReference databaseReference;
                 String email=binding.emailEt.getText().toString().trim();
                 String age=binding.ageEt.getText().toString().trim();
                 String blood=binding.bloodGroupEt.getText().toString().trim();
-                User user=new User(name, email, Integer.valueOf(age),blood);
-                userList.add(user);
+              /*  User user=new User(name, email, Integer.valueOf(age),blood);
+                userList.add(user);*/
                 userInsert(name,email,age,blood);
 
+
+     
+            }
+        });
+        
+        getUsers();
+    }
+
+    private void getUsers() {
+        DatabaseReference userRef=databaseReference.child("usersLevel");
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    userList.clear();
+                    for (DataSnapshot snapshot:dataSnapshot.getChildren()){
+                        User user=snapshot.getValue(User.class);
+                        userList.add(user);
+                        userAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
@@ -56,6 +88,15 @@ private DatabaseReference databaseReference;
     private void userInsert(String name, String email, String age, String blood) {
 
     User user=new User(name, email,Integer.valueOf(age),blood);
+    DatabaseReference userRef=databaseReference.child("usersLevel");
+    userRef.push().setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+        @Override
+        public void onComplete(@NonNull Task<Void> task) {
+            if (task.isSuccessful()){
+                Toast.makeText(getApplicationContext(), "Successful Insert", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
 }
     private void initRecyclerView() {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
